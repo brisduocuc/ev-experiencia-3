3424d5df2eb93874a3045156e46fdcc105c34e17
Brisa Sandoval H <bris.sandoval@duocuc.cl>
2026-06-23 20:18:16 -0400

    opcion 1 lista

--- cinema.py
+++ cinema.py
@@ -1,15 +1,3 @@
-# Variable película
-# titulo -> Ni vacío ni espacios en blanco
-# duracion -> Entero mayor a cero
-# calificacion -> Decimal entre 0.0 y 10.0 incluidos
-# disponible -> el sistema lo asigna
-pelicula_default = {
-    "titulo": "",
-    "duracion": 0,
-    "calificacion": 0.0,
-    "disponible": False
-}
-
 # Función que mostrará el menú en la consola, sin recibir ni retornar nada
 def mostrar_menu():
     print("========== MENÚ PRINCIPAL ==========")
@@ -32,24 +20,114 @@ def seleccionar_opcion():
             print("Ha ingresado una opción inválida, vuelva a intentarlo")
     return opcion
 
+# Función agregar película, recibe lista
+def agregar_pelicula(pelicula):
+    # Variables
+    titulo = validación_titulo()
+    duracion = validación_duracion()
+    calificacion = validación_calificacion()
+
+    # Condiciones de aceptación
+    mensajes_condiciones = []
+
+    if titulo == "":
+        mensajes_condiciones.append("El título no puede estar vacío")
+    
+    if titulo != "" and titulo.strip() == "":
+        mensajes_condiciones.append("El título no puede ser solo espacios en blanco")
+    
+    if duracion <= 0:
+        mensajes_condiciones.append("La duración debe ser mayor a cero")
+    
+    if calificacion <= 0.0  or calificacion >= 10.0:
+        mensajes_condiciones.append("La calificación debe ser un número decimal entre 0.0 y 10.0")
+    
+    # Validamos si cumplen con los criterios
+    if len(mensajes_condiciones) == 0:
+        pelicula["titulo"] = titulo
+        pelicula["duracion"] = duracion
+        pelicula["calificacion"] = calificacion
+
+        print("Se ha registrado correctamente la película:")
+        print(f"Título: {pelicula.get("titulo")}")
+        print(f"Duración: {pelicula.get("duracion")}")
+        print(f"Calificación: {pelicula.get("calificacion")}")
+
+        return pelicula
+    else:
+        for mensaje in mensajes_condiciones:
+            print(mensaje)
+        print("No se ha podido registrar la película")
+        return None
+
+
+# Validación título
+def validación_titulo():
+    while True:
+        titulo = input("Ingrese el nombre de la película a agregar: ")
+        if titulo.isdigit():
+            print("Debe ingresar un nombre valido")
+        else:
+            break
+    return titulo
+# Validación duración
+def validación_duracion():
+    while True:
+        try:
+            duracion = int(input("Ingrese la duración de la película: "))
+            if isinstance(duracion, int):
+                break
+        except ValueError:
+            print("Debe ingresar un número válido")
+    return duracion
+# Validación Calificación
+def validación_calificacion():
+    while True:
+        try:
+            calificacion = float(input("Ingrese la calificación de la película: "))
+            if isinstance(calificacion, float):
+                break
+        except ValueError:
+            print("Debe ingresar un número válido")
+    return calificacion
+
 # Sistema cinema
 def cinema():
-    mostrar_menu()
-    opcion = seleccionar_opcion()
+    # Inicializamos variable opcion
+    opcion = 0
+    lista_peliculas = []
 
-    # Opciones
-    match opcion:
-        case 1:
-            print("1")
-        case 2:
-            print("2")
-        case 3:
-            print("3")
-        case 4:
-            print("4")
-        case 5:
-            print("5")
-        case 6:
-            print("Gracias por usar el sistema. Vuelva Pronto")
+    while opcion != 6:
+        mostrar_menu()
+        opcion = seleccionar_opcion()
 
-print(seleccionar_opcion())
+        # Inicializamos variable película
+        # titulo -> Ni vacío ni espacios en blanco
+        # duracion -> Entero mayor a cero
+        # calificacion -> Decimal entre 0.0 y 10.0 incluidos
+        # disponible -> el sistema lo asigna
+        pelicula_default = {
+            "titulo": "",
+            "duracion": 0,
+            "calificacion": 0.0,
+            "disponible": False
+        }
+
+        # Opciones
+        match opcion:
+            case 1:
+                pelicula = agregar_pelicula(pelicula_default)
+                if pelicula is not None:
+                    lista_peliculas.append(pelicula)
+            case 2:
+                print("2")
+            case 3:
+                print("3")
+            case 4:
+                print("4")
+            case 5:
+                print("5")
+            case 6:
+                print("Gracias por usar el sistema. Vuelva Pronto")
+
+cinema()
